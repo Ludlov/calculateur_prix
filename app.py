70a5383c8b3f3298e9c83ecf517a568348f77c39
Ludovic PRIME <Ludovic.PRIME@PREMIUMG.INTERNE>
2025-03-12 18:22:47 +0100

    Mise à jour du calcul des incoterms et du transport

--- app.py
+++ app.py
@@ -80,4 +80,3 @@ if st.button("Calculer"):
             st.markdown(f"<p style='color:red; font-weight:bold;'>{key} : {value:.2f} €/kg</p>", unsafe_allow_html=True)
         else:
             st.write(f"{key} : {value:.2f} €/kg")
-
